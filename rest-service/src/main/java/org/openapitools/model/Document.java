70fe27531e1120be884638f4c5a627a39f1cb3f8
stani <stani1423@gmail.com>
2024-09-23 22:03:26 +0200

    Spring

---
+++ rest-service/src/main/java/org/openapitools/model/Document.java
@@ -0,0 +1,156 @@
+package org.openapitools.model;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import java.util.UUID;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import javax.annotation.Generated;
+
+/**
+ * Document
+ */
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-09-23T21:57:54.609887200+02:00[Europe/Berlin]", comments = "Generator version: 7.8.0")
+public class Document {
+
+  private UUID id;
+
+  private String username;
+
+  private String description;
+
+  private String uploadedDate;
+
+  public Document id(UUID id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Get id
+   * @return id
+   */
+  @Valid 
+  @Schema(name = "id", example = "550e8400-e29b-41d4-a716-446655440000", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("id")
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public Document username(String username) {
+    this.username = username;
+    return this;
+  }
+
+  /**
+   * Get username
+   * @return username
+   */
+  
+  @Schema(name = "username", example = "name", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("username")
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public Document description(String description) {
+    this.description = description;
+    return this;
+  }
+
+  /**
+   * Get description
+   * @return description
+   */
+  
+  @Schema(name = "description", example = "This is a description of the doc", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("description")
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public Document uploadedDate(String uploadedDate) {
+    this.uploadedDate = uploadedDate;
+    return this;
+  }
+
+  /**
+   * Get uploadedDate
+   * @return uploadedDate
+   */
+  
+  @Schema(name = "uploaded_date", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("uploaded_date")
+  public String getUploadedDate() {
+    return uploadedDate;
+  }
+
+  public void setUploadedDate(String uploadedDate) {
+    this.uploadedDate = uploadedDate;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Document document = (Document) o;
+    return Objects.equals(this.id, document.id) &&
+        Objects.equals(this.username, document.username) &&
+        Objects.equals(this.description, document.description) &&
+        Objects.equals(this.uploadedDate, document.uploadedDate);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, username, description, uploadedDate);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Document {\n");
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    username: ").append(toIndentedString(username)).append("\n");
+    sb.append("    description: ").append(toIndentedString(description)).append("\n");
+    sb.append("    uploadedDate: ").append(toIndentedString(uploadedDate)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
---
+++ rest-service/src/main/java/org/openapitools/model/DocumentContent.java
@@ -0,0 +1,108 @@
+package org.openapitools.model;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import java.util.UUID;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import javax.annotation.Generated;
+
+/**
+ * DocumentContent
+ */
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-09-23T21:57:54.609887200+02:00[Europe/Berlin]", comments = "Generator version: 7.8.0")
+public class DocumentContent {
+
+  private UUID id;
+
+  private String content;
+
+  public DocumentContent id(UUID id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Get id
+   * @return id
+   */
+  @Valid 
+  @Schema(name = "id", example = "550e8400-e29b-41d4-a716-446655440000", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("id")
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public DocumentContent content(String content) {
+    this.content = content;
+    return this;
+  }
+
+  /**
+   * Get content
+   * @return content
+   */
+  
+  @Schema(name = "content", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("content")
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    DocumentContent documentContent = (DocumentContent) o;
+    return Objects.equals(this.id, documentContent.id) &&
+        Objects.equals(this.content, documentContent.content);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, content);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class DocumentContent {\n");
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    content: ").append(toIndentedString(content)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
