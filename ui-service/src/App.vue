211574218ae41ff24cd724d3705895d3985c9ea4
SaraK13 <if22b237@technikum-wien.at>
2024-09-28 15:12:22 +0200

    changed App

--- ui-service/src/App.vue
+++ ui-service/src/App.vue
@@ -1,18 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <HelloWorld msg="Paperless"/>
+  <div class="container">
+    <file-list :files="uploadedFiles" @delete-file="deleteFile" @edit-file="editFile" />
+    <file-upload @files-uploaded="addFiles" />
+  </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { defineComponent } from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
+import { ref } from 'vue';
+import FileList from './components/FileList.vue';
+import FileUpload from './components/FileUpload.vue';
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+const uploadedFiles = ref<Array<{ name: string }>>([]);
+const addFiles = (files: Array<{ name: string }>) => {
+  uploadedFiles.value = [...uploadedFiles.value, ...files];
+};
+const deleteFile = (index: number) => {
+  uploadedFiles.value.splice(index, 1);
+};
+const editFile = (index: number) => {
+  // Handle the logic to edit the file, e.g., rename it
+  console.log(`Edit file at index ${index}`);
+};
 </script>
 
 <style>
@@ -24,4 +35,10 @@ export default defineComponent({
   color: #2c3e50;
   margin-top: 60px;
 }
+.container {
+  display: flex;
+  justify-content: center;
+  padding: 20px;
+}
 </style>
+
