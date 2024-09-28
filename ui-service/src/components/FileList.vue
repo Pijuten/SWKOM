2329512999e5688494f8326567ab785e01539ede
SaraK13 <if22b237@technikum-wien.at>
2024-09-28 15:15:33 +0200

    File upload and list

---
+++ ui-service/src/components/FileList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="file-list">
+    <h3>Uploaded files</h3>
+    <ul>
+      <li v-for="(file, index) in files" :key="index">
+        {{ file.name }}
+        <button @click="editFile(index)">Edit</button>
+        <button @click="deleteFile(index)">Delete</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { defineEmits, defineProps } from 'vue';
+  const props = defineProps({
+    files: {
+      type: Array as () => Array<{ name: string }>, // Define the type for the files
+      required: true
+    }
+  });
+  const emit = defineEmits(['delete-file', 'edit-file']);
+  const deleteFile = (index: number) => {
+    emit('delete-file', index);
+  };
+  const editFile = (index: number) => {
+    emit('edit-file', index);
+  };
+</script>
+
+<style scoped>
+  .file-list {
+    width: 60%;
+    border: 1px solid #2c3e50;
+    margin-right: 10px;
+  }
+</style>
