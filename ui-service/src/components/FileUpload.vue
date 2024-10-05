80182e6396d7bfdf1d8ee999abbb750d572a16d7
Fariba Rezai <if22b501@technikum.wien.at>
2024-10-05 17:01:34 +0200

    added dragging files

--- ui-service/src/components/FileUpload.vue
+++ ui-service/src/components/FileUpload.vue
@@ -3,14 +3,25 @@
     <h3>Upload a new File</h3>
     <input type="file" multiple @change="onFileSelect" />
     <p>Drag & Drop your files here or click to select</p>
+
+    <div class=" upload-zone"
+  @dragover.prevent= "onDragOver"
+         @dragenter="onDragEnter"
+  @dragleave= "onDragLeave"
+  @drop.prevent="onFileDrop">
+  </div>
+
   </div>
 </template>
 
 <script setup lang="ts">
   import { defineEmits } from 'vue';
+  import {valueOf} from "axios";
+  import {ref} from 'vue';
 
   // Emit files-uploaded event
   const emit = defineEmits(['files-uploaded']);
+  const isDragging= ref(false) //track dragging state
 
   // Handle file upload
   const onFileSelect = (event: Event) => {
@@ -21,24 +32,43 @@
     }
   };
   const onFileDrop = (event: DragEvent) => {
+   isDragging.value= false;
     const files = Array.from(event.dataTransfer?.files || []);
     emit('files-uploaded', files);
   };
-  const onDragOver = () => {
-    // TODO?
-    return;
+
+  //Handle drag over (allow dropping)
+  const onDragOver = (e: DragEvent) => {
+    e.preventDefault(); //allow dropping
+    e.dataTransfer!.dropEffect = 'copy';
   };
+
+  //Handle drag enter (show visual feedback)
+  const onDragEnter = () => {
+    isDragging.value= true;
+  }
+
+  //handle remove visual feedback
+  const onDragLeave = () => {
+    isDragging.value= false;
+  }
+
 </script>
 
 <style scoped>
   .upload-zone {
-    width: 25%;
-    padding: 20px;
+    position: relative;
+    width: 30%;
+    padding: 30px;
     border: 2px dashed #ccc;
     text-align: center;
     cursor: pointer;
+    transition: background-color 0.3s;
   }
   .upload-zone:hover {
     background-color: #f9f9f9;
   }
+
+
+
 </style>
