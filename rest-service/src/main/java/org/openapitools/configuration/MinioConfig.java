c8d0aad51f7be23767e7b09095eabaea87bb5fc5
SaraK13 <sarahlanikai@gmail.com>
2024-12-01 20:58:52 +0100

    MinIO config setup

---
+++ rest-service/src/main/java/org/openapitools/configuration/MinioConfig.java
@@ -0,0 +1,15 @@
+package org.openapitools.configuration;
+import io.minio.MinioClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinioConfig {
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint("http://localhost:9000")
+                .credentials("minioadmin", "minioadmin123")
+                .build();
+    }
+}
