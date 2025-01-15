94a7213e8325a39ab57e4a5146f94df544e87a3c
Fariba Rezai <if22b501@technikum.wien.at>
2025-01-15 23:07:25 +0100

    added log & ExceptionHandling in RabbitSender- & -ReceiverService

--- rest-service/src/main/java/org/openapitools/services/RabbitMQSenderService.java
+++ rest-service/src/main/java/org/openapitools/services/RabbitMQSenderService.java
@@ -1,7 +1,8 @@
 package org.openapitools.services;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.stereotype.Service;
-
+@Slf4j
 @Service
 public class RabbitMQSenderService {
     private final AmqpTemplate amqpTemplate;
@@ -11,6 +12,14 @@ public class RabbitMQSenderService {
     }
 
     public void sendToOcrQueue(String message) {
-        amqpTemplate.convertAndSend("ocr_queue", message);
+       try{
+           log.info("Sending message to ocr_queue: {}", message);
+           amqpTemplate.convertAndSend("ocr_queue", message);
+           log.info("Message sent successfully to ocr_queue");
+       }
+       catch(Exception e){
+           log.error("Error sending message to ocr_queue: {} - Error: {}", message,e.getMessage(), e);
+          throw new RuntimeException("Failed to send message to ocr_queue: " + e.getMessage());
+       }
     }
 }
--- rest-service/src/main/java/org/openapitools/services/RabbitReceiverService.java
+++ rest-service/src/main/java/org/openapitools/services/RabbitReceiverService.java
@@ -1,5 +1,6 @@
 package org.openapitools.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.openapitools.repositories.jpa.DocumentContentJPARepository;
 import org.openapitools.services.dto.DocumentContentDto;
 import org.springframework.amqp.AmqpRejectAndDontRequeueException;
@@ -7,7 +8,7 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
-
+@Slf4j
 @Component
 public class RabbitReceiverService {
     private final DocumentContentJPARepository documentContentJPARepository;
@@ -20,16 +21,26 @@ public class RabbitReceiverService {
     public void receiveResultMessage(String message) {
         try {
             String[] result = spiltAround_(message);
+
+            log.info("Received message: {}", message);
             documentContentJPARepository.save(new DocumentContentDto(UUID.fromString(result[0]), result[1]));
+            log.info("Document saved successfully!");
+
         } catch (Exception e) {
-            // Log the error and handle appropriately
-            System.err.println("Error processing message: " + message + ", Error: " + e.getMessage());
+          // Log the error and handle appropriately
+            log.error("Error processing message: {} - Error: {}", message, e.getMessage(),e);
             throw new AmqpRejectAndDontRequeueException("Failed to process message", e);
         }
     }
 
 
     public String[] spiltAround_(String message) {
+        try{
         return message.split("_");
+    }
+        catch(Exception e){
+        log.error("Error splitting message: {} - Error: {}", message, e.getMessage(),e);
+        throw new AmqpRejectAndDontRequeueException("Invalid message format for splitting", e);
+        }
     }
 }
